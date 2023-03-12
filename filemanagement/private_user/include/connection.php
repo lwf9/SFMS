9767d99e4d17c99bae45bcb4907ba992d0ea52fe
lwf9 <pagelwf3@gmail.com>
2023-03-12 17:24:07 +0800

    School File Management System

---
+++ filemanagement/private_user/include/connection.php
@@ -0,0 +1,7 @@
+<?php 
+$conn = mysqli_connect("sql308.epizy.com","epiz_31611347","E2vP4Y2oUBew","epiz_31611347_filemngsystem");
+
+if(!$conn){
+	die("Connection error: " . mysqli_connect_error());	
+}
+?>
